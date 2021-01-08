d85911a00043e487dbdc096ba98af10c47d35e73
Jovan <jovanmaks92@gmail.com>
2021-01-08 08:56:33 +0100

    Input Polling

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -1,6 +1,8 @@
 #pragma once    
 
-
+#ifdef HZ_DEBUG
+	#define HZ_ENABLE_ASSERTS
+#endif
 
 #ifdef HZ_ENABLE_ASSERTS
 	#define HZ_ASSERT(x, ...) { if(!(x)) { HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); raise(SIGTRAP); } }
@@ -9,6 +11,7 @@
 	#define HZ_ASSERT(x, ...)
 	#define HZ_CORE_ASSERT(x, ...)
 #endif
+
 
 
 
