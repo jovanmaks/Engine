affa8a5851a2504d49fd557f3837a4b60bd599c4
Jovan <jovanmaks92@gmail.com>
2021-01-09 16:22:07 +0100

    Shaders

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -41,6 +41,33 @@ namespace Hazel {
 
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+
+		std::string vertexSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) in vec3 a_Position;
+			out vec3 v_Position;
+			void main()
+			{
+				v_Position = a_Position;
+				gl_Position = vec4(a_Position, 1.0);	
+			}
+		)";
+
+		std::string fragmentSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) out vec4 color;
+			in vec3 v_Position;
+			void main()
+			{
+				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+			}
+		)";
+
+		m_Shader.reset(new Shader(vertexSrc, fragmentSrc));
+
+
 		glEnableVertexAttribArray(0);
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
 
@@ -94,6 +121,8 @@ namespace Hazel {
             glClearColor(.1f,.1f,.1f,1);
             glClear(GL_COLOR_BUFFER_BIT);
 
+            m_Shader->Bind();
+
             glBindVertexArray(m_VertexArray);
 			glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
             
@@ -102,6 +131,7 @@ namespace Hazel {
 
 
             m_ImGuiLayer->Begin();
+
 			for (Layer* layer : m_LayerStack)
 				layer->OnImGuiRender();
 			m_ImGuiLayer->End();
--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -11,7 +11,7 @@
 
 #include "Hazel/Events/ApplicationEvent.h"
 
-
+#include "Hazel/Renderer/Shader.h"
 
 
 
@@ -44,6 +44,7 @@ namespace Hazel{
             LayerStack m_LayerStack;
 
             unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+            std::unique_ptr<Shader> m_Shader;
         private:
 		    static Application* s_Instance;
 
