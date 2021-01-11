b7863944b834b42ed94612a62d5544f33ccdaa21
Jovan <jovanmaks92@gmail.com>
2021-01-11 14:38:56 +0100

    Vertex Arrays

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -13,7 +13,7 @@
 
 #include "Hazel/Renderer/Shader.h"
 #include "Hazel/Renderer/Buffer.h"
-
+#include "Hazel/Renderer/VertexArray.h"
 
 
 namespace Hazel{
@@ -44,10 +44,11 @@ namespace Hazel{
             bool m_Running = true;
             LayerStack m_LayerStack;
 
-            unsigned int m_VertexArray;
-            std::unique_ptr<Shader> m_Shader;
-            std::unique_ptr<VertexBuffer> m_VertexBuffer;
-		    std::unique_ptr<IndexBuffer> m_IndexBuffer;
+            std::shared_ptr<Shader> m_Shader;
+		std::shared_ptr<VertexArray> m_VertexArray;
+
+		std::shared_ptr<Shader> m_BlueShader;
+		std::shared_ptr<VertexArray> m_SquareVA;
         private:
 		    static Application* s_Instance;
 
