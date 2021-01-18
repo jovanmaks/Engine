138c44bbfd98b19020ad42622f9424f0825f8c67
Jovan <jovanmaks92@gmail.com>
2021-01-18 22:15:18 +0100

    Batching - textures

--- Hazel/src/Platform/OpenGL/OpenGLShader.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLShader.cpp
@@ -194,12 +194,17 @@ namespace Hazel {
 		HZ_PROFILE_FUNCTION();
 		UploadUniformInt(name, value);
 	}
-	
+
 	void OpenGLShader::SetFloat(const std::string& name, float value)
 	{
 		HZ_PROFILE_FUNCTION();
 
 		UploadUniformFloat(name, value);
+	}
+
+	void OpenGLShader::SetIntArray(const std::string& name, int* values, uint32_t count)
+	{
+		UploadUniformIntArray(name, values, count);
 	}
 
 	void OpenGLShader::SetFloat3(const std::string& name, const glm::vec3& value)
@@ -227,6 +232,12 @@ namespace Hazel {
 		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
 		glUniform1i(location, value);
 	} 
+
+	void OpenGLShader::UploadUniformIntArray(const std::string& name, int* values, uint32_t count)
+	{
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniform1iv(location, count, values);
+	}
 
 	void OpenGLShader::UploadUniformFloat(const std::string& name, float value)
 	{
