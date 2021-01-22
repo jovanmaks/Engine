9f0e08cdb8848d81b85b2ce829f36d99be92aa3c
Jovan <jovanmaks92@gmail.com>
2021-01-22 18:25:40 +0100

    Hazel-Editor

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -67,6 +67,11 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("/home/demijan/Projects/Engine/build/Sandbox/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/demijan/Projects/Engine/build/Hazel-Editor/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
