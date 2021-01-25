8f8254e0d1abbcbb677276b7788d9406baaee661
Jovan <jovanmaks92@gmail.com>
2021-01-25 10:24:54 +0100

    fixes

--- Hazel/src/Hazel/Core/Log.h
+++ Hazel/src/Hazel/Core/Log.h
@@ -3,8 +3,11 @@
 
 #include "Hazel/Core/Base.h"
 
+// This ignores all warnings raised inside External headers
+#pragma warning(push, 0)
 #include <spdlog/spdlog.h>
 #include <spdlog/fmt/ostr.h>
+#pragma warning(pop)
 
 namespace Hazel
 {
--- Hazel/src/Hazel/Scene/SceneCamera.cpp
+++ Hazel/src/Hazel/Scene/SceneCamera.cpp
@@ -10,14 +10,7 @@ namespace Hazel {
 		RecalculateProjection();
 	}
 
-	void SceneCamera::SetOrthographic(float size, float nearClip, float farClip)
-	{
-		m_ProjectionType = ProjectionType::Orthographic;
-		m_OrthographicSize = size;
-		m_OrthographicNear = nearClip;
-		m_OrthographicFar = farClip;
-		RecalculateProjection();
-	}
+	
 
 	void SceneCamera::SetPerspective(float verticalFOV, float nearClip, float farClip)
 	{
@@ -31,6 +24,15 @@ namespace Hazel {
 	void SceneCamera::SetViewportSize(uint32_t width, uint32_t height)
 	{
 		m_AspectRatio = (float)width / (float)height;
+		RecalculateProjection();
+	}
+	
+	void SceneCamera::SetOrthographic(float size, float nearClip, float farClip)
+	{
+		m_ProjectionType = ProjectionType::Orthographic;
+		m_OrthographicSize = size;
+		m_OrthographicNear = nearClip;
+		m_OrthographicFar = farClip;
 		RecalculateProjection();
 	}
 
